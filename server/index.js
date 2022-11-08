403fc07ec010c9e7ac428e9da97f79e6a62c220f
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2022-11-08 13:50:48 -0300

    server initial setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
-app.listen(PORT)
+//middleware
+app.use(express.json) //req.body
+app.use(cors())
+
+//ROUTES
+
+app.listen(PORT, () => {
+    console.log(`Server is running in port ${PORT}`);
+})
