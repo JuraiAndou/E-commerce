bc12a735d185dbeb7c1fa1392da06980a2faf39e
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2022-11-13 13:34:27 -0300

    Client: Dashboard route added

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,10 +1,42 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 const Dashbord = (props) => {
+
+    const [name, setName] = useState("")
+
+    async function getName() {// Gets the user name from the dashboard Server Route
+        try {
+            const response = await fetch("http://localhost:5000/dashboard/", {
+                method: 'GET',
+                headers: { token: localStorage.token }
+            })
+
+            const parseRes = await response.json()
+
+            setName(parseRes.user_name)
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    const logout = (e) => {
+        e.preventDefault()// Prevents a refresh
+        localStorage.removeItem('token')
+        props.setAuth(false)
+    }
+
+    useEffect(() => {// Called everytime the component is reached
+        getName()
+    })
+
     return (
         <Fragment>
             <h1>Dashboard</h1>
-            <button onClick={() => props.setAuth(false)}>Logout</button>
+            <p>Hello <strong>{name.split(' ')[0]}</strong></p>
+            <button
+                className="btn btn-primary"
+                onClick={e => logout(e)}
+            >Logout</button>
         </Fragment>
     )
 }
