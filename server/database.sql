63991acadccaef78755d48a8ba8f7ce3415b4cc6
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2022-11-13 15:38:12 -0300

    Update database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,10 +1,11 @@
-CREATE DATABASE "e-commerce";
+--CREATE DATABASE "e-commerce";
 
 CREATE TABLE "users"(
     user_id SERIAL PRIMARY KEY,--this can be changed to a uuid via extentions
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL
+    user_password VARCHAR(255) NOT NULL,
+    user_admin BOOLEAN DEFAULT FALSE
 );
 
 --insert users
