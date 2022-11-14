9e3106100f9de441f1d78c3cdf1288bdef9a517b
BrunoEskinaziUFC <brunoeskinazi@alu.ufc.br>
2022-11-14 03:23:42 -0300

    Insert de Categoria

--- client/src/components/DashboardAdmin.jsx
+++ client/src/components/DashboardAdmin.jsx
@@ -32,10 +32,85 @@ const DashbordAdmin = (props) => {
         getName()
     }, [])
 
+    const [productData, setPoductData] = useState({
+        descricao: "",
+        preco: 0,
+        quantidade: 0
+    });
+
+    const onChangeProduct = (e)=>{
+        setPoductData({...productData, [e.target.name]: e.target.value})
+    }
+
+    const onSubmitProduct = async(e) =>{
+        e.preventDefault();
+        try {//console.log(productData);
+        const body = productData;
+        console.log(body);
+
+        const response = await fetch('http://localhost:5000/product/add-product', {
+                method: 'POST',
+                headers: { token: localStorage.token, 
+                           'Content-Type': 'application/json' 
+                         },
+                body: JSON.stringify(body)
+            })
+        const parseRes = await response.json()
+            
+        console.log(parseRes);
+        }catch(err){
+            console.error(err.message);
+        }
+    }
+
+    const [categoryData, setCategoryData] = useState({
+        descricao: "",
+    });
+
+    const onChangeCategory = (e)=>{
+        setCategoryData({...categoryData, [e.target.name]: e.target.value})
+    }
+
+    const onSubmitCategory = async(e) =>{
+        e.preventDefault();
+        try {//console.log(productData);
+        const body = categoryData;
+        console.log(body);
+
+        const response = await fetch('http://localhost:5000/category/add-category', {
+                method: 'POST',
+                headers: { token: localStorage.token, 
+                           'Content-Type': 'application/json' 
+                         },
+                body: JSON.stringify(body)
+            })
+        const parseRes = await response.json()
+            
+        console.log(parseRes);
+        }catch(err){
+            console.error(err.message);
+        }
+    }
+
     return (
         <Fragment>
             <h1>Dashboard Admin</h1>
             <p>Hello <strong>{name.split(' ')[0]}</strong></p>
+            
+            Adicionar novo produto: 
+            <form onSubmit={onSubmitProduct}>
+                <input type="text" name="descricao" placeholder="Descrição" value={productData.descricao} onChange={e=>{onChangeProduct(e)}}/><br/>
+                <input type="text" name="preco" placeholder="Preço" value={productData.preco} onChange={e=>{onChangeProduct(e)}}/><br/>
+                <input type="text" name="quantidade" placeholder="Quantidade" value={productData.quantidade} onChange={e=>{onChangeProduct(e)}}/><br/>
+                <input type="submit"/>
+            </form>
+            
+            Adicionar nova categoria: 
+            <form onSubmit={onSubmitCategory}>
+                <input type="text" name="descricao" placeholder="Descrição" value={categoryData.descricao} onChange={e=>{onChangeCategory(e)}}/><br/>
+                <input type="submit"/>
+            </form>
+
             <Link to="/edit" className="btn">Edit Profile</Link>
             <button
                 className="btn btn-primary"
