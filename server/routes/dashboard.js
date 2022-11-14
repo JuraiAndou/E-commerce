e4cbde37dea1027add510e4ed7971c6b6442fa1a
BrunoEskinaziUFC <brunoeskinazi@alu.ufc.br>
2022-11-14 14:20:36 -0300

    Fixed EditProfile
    Fixed the EditProfile postgreSQL query

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -44,16 +44,16 @@ router.post('/update', authorization, async (req, res) => {
          * @TODO Change this to a user DAO
          */
         
-        const {user_name, user_email, user_address, user_password} = await req.body;
+        const {user_name, user_email, user_adress, user_password} = await req.body;
         //console.log(newName);
         // Criptografar a senha
         const saltRound = 10
         const salt = await bcrypt.genSalt(saltRound)
 
         const bcryptPassword = await bcrypt.hash(user_password, salt)
-
-        const user = await pool.query('UPDATE users SET user_name = $1, user_email = $2, user_adress = $3 user_password = $4 WHERE user_id = $5 RETURNING user_name, user_email', 
-        [user_name, user_email, user_address, bcryptPassword, req.user.id])
+        console.log(user_name, user_email, user_adress, user_password);
+        const user = await pool.query('UPDATE users SET user_name = $1, user_email = $2, user_adress = $3, user_password = $4 WHERE user_id = $5 RETURNING user_name, user_email, user_adress, user_password', 
+        [user_name, user_email, user_adress, bcryptPassword, req.user.id])
 
         res.json(user.rows[0])
     } catch (err) {
