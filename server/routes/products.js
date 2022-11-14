0dbb15573218179e34edc1ec7c9e241eefc2fe1c
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2022-11-14 01:06:01 -0300

    Update products.js

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,7 +3,7 @@ const pool = require('../dbConfig')
 const authorization = require('../middleware/authorization')
 const priviledge = require('../middleware/isAdmin')
 
-router.get('/add-product', authorization, priviledge, async (req, res) => {
+router.post('/add-product', authorization, priviledge, async (req, res) => {
     try {
         const { descricao, preco, quantidade } = req.body
 
@@ -24,9 +24,28 @@ router.get('/add-product', authorization, priviledge, async (req, res) => {
     }
 })
 
-router.get('/add-product', authorization, priviledge, async (req, res) => {
+router.get('/get-products', authorization, priviledge, async (req, res) => {
     try {
+        //res.json(req.user)
+
+        /**
+         * @TODO Change this to a user DAO
+         */
+        const product = await pool.query('SELECT * FROM produto')
+
+        res.json(product.rows)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json('Server Error')
+    }
+})
+
+router.post('/update-product', authorization, priviledge, async (req, res) => {
+    try {
+        
         const { descricao, preco, quantidade } = req.body
+
+        const { id } = req.params.id_prod
 
         /**
          * @TODO Change this to a user DAO
@@ -38,11 +57,11 @@ router.get('/add-product', authorization, priviledge, async (req, res) => {
         ])
 
         res.json(newProduct.rows)
-
     } catch (err) {
         console.error(err.message);
         res.status(500).json('Server Error')
     }
 })
+
 
 module.exports = router
