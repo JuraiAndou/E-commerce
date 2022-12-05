67e4fc153fa54aeafbb2c7a37d9262c208388aec
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2022-12-05 04:47:37 -0300

    data

--- client/src/components/DashboardAdmin.jsx
+++ client/src/components/DashboardAdmin.jsx
@@ -1,7 +1,8 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { toast } from 'react-toastify';
-
+import moment from 'moment'
+import reactMoment from 'react-moment'
 
 
 
@@ -89,7 +90,7 @@ const DashbordAdmin = (props) => {
                 const parseRes = await response.json()
                 toast.success('Category ' + categoryData.descricao + ' added')
 
-    
+
             } else {
                 toast.error('Product description empty')
             }
@@ -154,7 +155,7 @@ const DashbordAdmin = (props) => {
 
             const parseRes = await response.json()
             setClienteCompras(parseRes)
-           
+
         } catch (err) {
             console.error(err.message);
         }
@@ -164,26 +165,26 @@ const DashbordAdmin = (props) => {
         const elem = document.getElementById("clientSelect");
         let valor = elem.value
         getClienteCompras(valor)
-        
+
     }
 
-    function showClientSales (){       
+    function showClientSales() {
         return clienteCompras.map((ind) => {
             return (
                 <Fragment>
                     <tr key={ind.id} style={table_style}>
-                    <td style={table_style}><button className="btn btn-danger" onClick={e => { onRemoveSale(e, ind.id)}} >   X  </button>   {ind.id}</td>
-                    <td style={table_style}>R${ind.preco_final}</td>
-                    <td style={table_style}>{ind.data} </td>
-                   </tr>
-                </Fragment>   
+                        <td style={table_style}><button className="btn btn-danger" onClick={e => { onRemoveSale(e, ind.id) }} >   X  </button>   {ind.id}</td>
+                        <td style={table_style}>R${ind.preco_final}</td>
+                        <td style={table_style}>{ind.data} </td>
+                    </tr>
+                </Fragment>
             )
         })
     }
     //----------Exclusão de compra cliente-----------
-    async function onRemoveSale(e,id_venda){
+    async function onRemoveSale(e, id_venda) {
         try {
-            const response = await fetch("http://localhost:5000/sales/remove-venda-client?prod=" + id_venda,{
+            const response = await fetch("http://localhost:5000/sales/remove-venda-client?prod=" + id_venda, {
                 method: 'POST',
                 headers: { token: localStorage.token },
             })
@@ -219,7 +220,7 @@ const DashbordAdmin = (props) => {
         return Relatorio.map((rel) => {
             return (
                 <tr key={rel[0]} style={table_style}>
-                    <td style={table_style}>{rel[1]}</td> 
+                    <td style={table_style}>{rel[1]}</td>
                     <td style={table_style}>{rel[0]}</td>
                     <td style={table_style}>{rel[2]}</td>
                 </tr>
@@ -274,17 +275,20 @@ const DashbordAdmin = (props) => {
     //---------------Relatorio Vendas por Dias----------------
 
     const [RelatorioVendas, setRelatorioVendas] = useState([])
+    const [VInintialDate, setVInitialDate] = useState([])
+    const [VFinalDate, setVFinalDate] = useState([])
 
     async function getRelatorioVendas() {
+        console.log('data init:', VInintialDate, '\ndate final:', VFinalDate)
         try {
             const result = await fetch("http://localhost:5000/sales/get-vendas-per-day?" + new URLSearchParams({
-                date_int: '2022-02-01',
-                date_fnl: '2022-12-03'
+                date_int: VInintialDate,
+                date_fnl: VFinalDate
             }), {
                 method: 'GET',
                 headers: { token: localStorage.token }
             })
-            
+
             const parseRes = await result.json()
             setRelatorioVendas(parseRes)
         } catch (err) {
@@ -295,7 +299,6 @@ const DashbordAdmin = (props) => {
 
     function showRelatorioVendas() {
         return RelatorioVendas.map((rel, i) => {
-            console.log(rel)
             return (
                 <tr key={i} style={table_style}>
                     <td style={table_style}>{rel[0]}</td>
@@ -303,6 +306,20 @@ const DashbordAdmin = (props) => {
                 </tr>
             )
         })
+    }
+
+    function onChageVInitialDate(e) {
+        const newDate = moment(new Date(e.target.value)).format('YYYY-MM-DD')
+        setVInitialDate(newDate)
+
+        getRelatorioVendas()
+    }
+
+    function onChageVFinalDate(e) {
+        const newDate = moment(new Date(e.target.value)).format('YYYY-MM-DD')
+        setVFinalDate(newDate)
+
+        getRelatorioVendas()
     }
 
 
@@ -321,7 +338,7 @@ const DashbordAdmin = (props) => {
     //<br/><input type="text" name="categoria" placeholder="Categoria" value={productData.categoria} onChange={e=>{onChangeProduct(e)}}/><br/>
     return (
         <Fragment>
-            <br/>
+            <br />
             <p>Hello <strong>{name.split(' ')[0]}</strong></p>
 
             Adicionar novo produto:
@@ -338,29 +355,29 @@ const DashbordAdmin = (props) => {
                         ))
                     }
                 </select>
-                <br/>
-                <input type="submit" className="btn btn-primary"/>
+                <br />
+                <input type="submit" className="btn btn-primary" />
             </form>
             <br />
 
             Adicionar nova categoria:
             <form onSubmit={onSubmitCategory}>
                 <input type="text" name="descricao" placeholder="Descrição" value={categoryData.descricao} onChange={e => { onChangeCategory(e) }} /><br />
-                <input type="submit" className="btn btn-primary"/>
+                <input type="submit" className="btn btn-primary" />
             </form>
             <br />
             <br />
 
-            <h3><strong>Relatorio de vendas pra o cliente:</strong></h3>
+            <h3><strong>Vendas por cliente:</strong></h3>
             Cliente: <select id="clientSelect" name="cliente" onChange={e => { onClienteChange(e) }} className="form-select" aria-label="Default select example">
-            <option value="---" disabled selected hidden >Selecione um cliente</option>                {
+                <option value="---" disabled selected hidden >Selecione um cliente</option>                {
                     cliente.length > 0 &&
                     cliente.map((client) => (
                         <option key={client.user_id} value={client.user_id}>{client.user_email}</option>
                     ))
                 }
             </select>
-            <br/>
+            <br />
             <Fragment>
                 <table width="400" cellPadding="5"
                     style={table_style} className="table table-dark table-striped-columns">
@@ -382,7 +399,7 @@ const DashbordAdmin = (props) => {
             <br />
             <h2 > <strong>Relatório</strong></h2>
             <Fragment>
-                <h5>Compras feitas por cliente</h5>
+                <h5>Total de Compras por cliente</h5>
                 <table width="400" cellPadding="5"
                     style={table_style}>
                     <thead>
@@ -416,7 +433,9 @@ const DashbordAdmin = (props) => {
             </Fragment>
             <br />
             <Fragment>
-                <h5>Produtos fora de estoque</h5>
+                <h5>Vendas por dia</h5>
+                    data inicial: <input id="data-init" type='date' value={VInintialDate} onChange={e => {onChageVInitialDate(e)}}/>
+                    data final: <input id="data-final" type="date" value={VFinalDate} onChange={e => {onChageVFinalDate(e)}}/>
                 <table width="400" cellPadding="5"
                     style={table_style}>
                     <thead>
@@ -431,6 +450,8 @@ const DashbordAdmin = (props) => {
                 </table>
             </Fragment>
             <br />
+
+            {/* Download Relatório */}
             <Fragment>
                 <div className="btnDiv">
                     <button id="downloadBtn" value="download" onClick={() => {
@@ -438,17 +459,17 @@ const DashbordAdmin = (props) => {
                         text.push(`\t-----[Compras Feitas por Cliente]-----\n`)
                         for (let i = 0; i < Relatorio.length; i++) {
                             const rel = Relatorio[i];
-                            text.push(`|id:`,rel[0], '|\t')
-                            text.push(`|nome:`,rel[1], '|\t')
-                            text.push(`|quantidade de compras:`,rel[2], '|\n')
+                            text.push(`|id:`, rel[0], '|\t')
+                            text.push(`|nome:`, rel[1], '|\t')
+                            text.push(`|quantidade de compras:`, rel[2], '|\n')
                         }
 
                         text.push(`\n\n\t-----[Produtos Fora de Estoque]-----\n`)
                         for (let i = 0; i < RelatorioP.length; i++) {
                             const rel = RelatorioP[i];
-                            text.push(`|id:`,rel[0], '|\t')
-                            text.push(`|Descrição:`,rel[1], '|\t')
-                            text.push(`|Preço:R$`,rel[2], '|\n')
+                            text.push(`|id:`, rel[0], '|\t')
+                            text.push(`|Descrição:`, rel[1], '|\t')
+                            text.push(`|Preço:R$`, rel[2], '|\n')
                         }
 
                         const file = new Blob(text, { type: 'text/plain' })
